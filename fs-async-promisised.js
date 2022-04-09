9ae207e65ffb7268ab51c22a728d3e684b91bfff
LokeshTiwari004 <lokeshtiwari001vns@gmail.com>
2022-04-09 17:20:34 +0530

    'redo commenting'

--- fs-async-promisised.js
+++ fs-async-promisised.js
@@ -30,15 +30,15 @@ const write = (path, data) => {
   })
 }
 
-// console.log(1);
-// read('./content/first.txt', [])
-//   .then((value) => {
-//     return read('./content/second.txt', value)
-//   })
-//   .then((value) => {
-//     return write('./content/result-async-promisised.txt', value)
-//   })
-// console.log(2);
+console.log(1);
+read('./content/first.txt', [])
+  .then((value) => {
+    return read('./content/second.txt', value)
+  })
+  .then((value) => {
+    return write('./content/result-async-promisised.txt', value)
+  })
+console.log(2);
 
 
 
